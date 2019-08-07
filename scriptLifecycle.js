75707d7328ad8bae6be1041a6367e35a0ab5fd39
Jochem Geussens <Jochem.Geussens@hotmail.com>
2019-08-07 20:14:11 +0200

    1) Added better error handling 2) Extended the readme a bit

--- scriptLifecycle.js
+++ scriptLifecycle.js
@@ -2,8 +2,14 @@ const scriptHelpers = require('./scriptHelpers');
 const {getActiveScript, getActiveScriptName, setScript} = require('./globalState');
 
 function onStart() {
-    if (getActiveScript() && getActiveScript().onStart) {
-        getActiveScript().onStart(scriptHelpers(getActiveScriptName().replace(__dirname, '')));
+    const helpers = scriptHelpers(getActiveScriptName().replace(__dirname, ''));
+    try {
+        if (getActiveScript() && getActiveScript().onStart) {
+            getActiveScript().onStart(helpers);
+        }
+    }
+    catch(error) {
+        helpers.log(error.message + error.stack || error);
     }
 }
 
